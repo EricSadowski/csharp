d08866fd55abc99e1b5cab800fb66d8a9a597f00
EricSadowski <118220751+EricSadowski@users.noreply.github.com>
2024-02-13 08:56:20 -0500

    update

---
+++ IceCream/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IceCream
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List <Flavor> flavors = new List<Flavor>();
+            //foreach (Flavor flavor in Flavors) { }
+            flavors.Add(new Flavor("Mint Chocolate"));
+            flavors.Add(new Flavor("Chocolate"));
+            flavors.Add(new Flavor("Vanilla"));
+            flavors.Add(new Flavor("Rocky Road"));
+            lvFlavors.ItemsSource = flavors;
+        }
+    }
+
+   class Flavor
+    {
+        public string Name { get; set; } 
+        
+        public Flavor(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ People/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace People
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+
+        }
+        private ObservableCollection<People> peopleList = new ObservableCollection<People>();
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+
+        private void LoadData()
+        {
+            const string dataFile = @"..\..\available.txt";
+            if (File.Exists(dataFile))
+            {
+
+                
+                IEnumerable<string> allLines = File.ReadLines(dataFile);
+                foreach (string line in allLines)
+                {
+                   string[] items = line.Split(';');
+                    string name = items[0];
+                    int age = int.Parse(items[1]);
+                    peopleList.Add(new People(name, age));
+                }
+                lvPeople.ItemsSource = peopleList;
+            }
+        }
+
+        class People
+        {
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public People(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            public override string ToString()
+            {
+                return Name + " " + Age;
+            }
+        }
+
+        private void btnAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            int age = int.Parse(txtBoxAge.Text);
+            string name = txtBoxName.Text;
+            People guy = new People(name, age);
+            peopleList.Add(guy);
+           // lvPeople.Items.Add(guy);
+        }
+    }
+}
